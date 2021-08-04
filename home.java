3080fcfbcd562f30525678550afe76f776f41701
dileep5121 <58946759+dileep5121@users.noreply.github.com>
2021-08-04 15:38:29 +0530

    Update home.java

--- home.java
+++ home.java
@@ -1,32 +1,17 @@
-import java.io.*;
+class PalindromeExample{  
+ public static void main(String args[]){  
+  int r,sum=0,temp;    
+  int n=454;//It is the number variable to be checked for palindrome  
   
-// Java code to demonstrate right star triangle
-public class GeeksForGeeks {
-    // Function to demonstrate printing pattern
-    public static void StarRightTriangle(int n)
-    {
-        int a, b;
-  
-        // outer loop to handle number of rows
-        // k in this case
-        for (a = 0; a < n; a++) {
-  
-            // inner loop to handle number of columns
-            // values changing acc. to outer loop
-            for (b = 0; b <= a; b++) {
-                // printing stars
-                System.out.print("* ");
-            }
-  
-            // end-line
-            System.out.println();
-        }
-    }
-  
-    // Driver Function
-    public static void main(String args[])
-    {
-        int k = 5;
-        StarRightTriangle(k);
-    }
-}
+  temp=n;    
+  while(n>0){    
+   r=n%10;  //getting remainder  
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum)    
+   System.out.println("palindrome number ");    
+  else    
+   System.out.println("not palindrome");    
+}  
+}  
