691d9abf85bf1426656ca6592e2af7e281499cc9
dileep5121 <58946759+dileep5121@users.noreply.github.com>
2021-08-04 15:44:59 +0530

    Update home.java

--- home.java
+++ home.java
@@ -1,18 +1,5 @@
-class home
-{
-   public static void main(String args[]) 
-    {   
-       
-      Scanner s= new Scanner(System.in);
-        
-         System.out.println("Enter the width of the Triangle:");
-         double b= s.nextDouble();
- 
-         System.out.println("Enter the height of the Triangle:");
-          double h= s.nextDouble();
- 
-                  //Area = (width*height)/2
-      double area=(b*h)/2;
-      System.out.println("Area of Triangle is: " + area);      
-   }
-}
+class home{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
